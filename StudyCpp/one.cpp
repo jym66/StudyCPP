597eec4318f010ab787a253841dc4bff68f98516
Hello <Hello>
2020-09-19 11:53:02 +0800

    add

---
+++ StudyCpp/one.cpp
@@ -0,0 +1,8 @@
+//
+//  one.cpp
+//  StudyCpp
+//
+//  Created by python on 2020/9/19.
+//
+
+#include "one.hpp"
---
+++ StudyCpp/one.hpp
@@ -0,0 +1,13 @@
+//
+//  one.hpp
+//  StudyCpp
+//
+//  Created by python on 2020/9/19.
+//
+
+#ifndef one_hpp
+#define one_hpp
+
+#include <stdio.h>
+
+#endif /* one_hpp */
